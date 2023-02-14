1a880b3d35184f76f5a668ee5ecdfb4fdd808b6f
Samuellmk <samuel_leong@outlook.com>
2023-02-14 11:05:51 +0800

    updated app with gradient bg

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import Home from './home';
+import UrlSearch from './urlSearch';
 
 export function App() {
   return (
-    <>
-      <Home />
+    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <UrlSearch />
       <div />
-    </>
+    </div>
   );
 }
 
