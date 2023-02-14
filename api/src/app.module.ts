14d9d8b2f7f990e1ab782d384580b2329b8ad40c
Samuellmk <samuel_leong@outlook.com>
2023-02-14 14:19:02 +0800

    include prismaModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UrlModule } from './url/url.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UrlModule],
+  imports: [UrlModule, PrismaModule],
 })
 export class AppModule {}
