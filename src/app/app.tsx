f0dfee637839ece41f6c5792f08f218ad67f52e2
Samuellmk <samuel_leong@outlook.com>
2023-02-14 10:40:23 +0800

    init - frontend

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import Home from './home';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="url-shortener" />
+      <Home />
       <div />
     </>
   );
