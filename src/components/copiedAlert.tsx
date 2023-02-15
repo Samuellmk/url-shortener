c64202af06968e0791848aa2b823595a3988b552
Samuellmk <samuel_leong@outlook.com>
2023-02-15 17:11:45 +0800

    updated copiedAlert

--- src/components/copiedAlert.tsx
+++ src/components/copiedAlert.tsx
@@ -9,11 +9,10 @@ export const CopiedAlert = (props: Props) => {
     <div
       className={`${
         props.isVisible
-          ? 'translate-y-0 '
-          : '-translate-y-full -top-full'
+          ? 'translate-y-0 top-3'
+          : '-translate-y-full -top-3'
       } ease-in-out duration-500
-        rounded-md 
-        top-3 bg-white shadow-md px-5 py-3 fixed w-fit z-40 `}
+        rounded-md bg-white shadow-md px-5 py-3 fixed w-fit z-40`}
     >
       <div className="flex gap-5 items-center">
         Copied Text{' '}
